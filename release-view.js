a3a0b02d91fc3261bf140cbf2736d83bf92d404a
Edward Seufert <edwardseufert@Edwards-MBP.home>
2020-06-11 20:59:33 -0400

    more buildout

--- release-view.js
+++ release-view.js
@@ -8,8 +8,8 @@ import Select from '../../coreView/common/select-input';
 
 
 export default function PMReleaseView({containerState, itemState, appPrefs, onListLimitChange,
-	onSearchChange, onSearchClick, onPaginationClick, onOrderBy, openDeleteModal, 
-	closeModal, onModify, onDelete, onEditRoles, inputChange, session}) {
+	onSearchChange, onSearchClick, onPaginationClick, onOrderBy, onOption, 
+	closeModal, inputChange, goBack, session}) {
 
     let columns = [];
     if (itemState.prefLabels != null && itemState.prefLabels.PM_RELEASE_PAGE != null) {
@@ -18,10 +18,19 @@ export default function PMReleaseView({containerState, itemState, appPrefs, onLi
     let group = "TABLE1";
     
     let header = "";
+    let parent = "";
+    if (itemState.parent != null) {
+		parent = itemState.parent.name;
+    }
+    
 	if (itemState.prefTexts.PM_RELEASE_PAGE != null && itemState.prefTexts.PM_RELEASE_PAGE.PM_RELEASE_PAGE_HEADER != null) {
 		header = itemState.prefTexts.PM_RELEASE_PAGE.PM_RELEASE_PAGE_HEADER.value;
 	}
 	
+	if (goBack != null && parent != null && parent != "") {
+		header = <div>{header} : <a onClick={() => goBack()} aria-hidden="true">{parent}</a></div>;
+	}
+    
 	let deleteModalHeader = "Delete ";
 	if (containerState.selected != null && containerState.selected.name != null) {
 		deleteModalHeader += containerState.selected.name;
@@ -36,7 +45,8 @@ export default function PMReleaseView({containerState, itemState, appPrefs, onLi
     			<ListBuilder
 		  	      	containerState={containerState}
 		  	      	header={header}
-		  	      	items={itemState.items}
+    				parent={parent}
+    				items={itemState.items}
 		  	      	itemCount={itemState.itemCount}
 		  	      	listStart={itemState.listStart}
 		  	      	listLimit={itemState.listLimit}
@@ -47,10 +57,7 @@ export default function PMReleaseView({containerState, itemState, appPrefs, onLi
 		  	      	onSearchClick={onSearchClick}
 		  	      	onPaginationClick={onPaginationClick}
 		  			onOrderBy={onOrderBy}
-	  				onHeader={onModify}
-	  				onOption1={onModify}
-	  				onOption2={openDeleteModal}
-	  				onOption3={onEditRoles}
+	  				onOption={onOption}
 		  			orderCriteria={itemState.orderCriteria}
 	  				searchCriteria={itemState.searchCriteria}
 		  	      />
@@ -58,6 +65,7 @@ export default function PMReleaseView({containerState, itemState, appPrefs, onLi
 	    		<Table
 	    			containerState={containerState}
 	    			header={header}
+	    			parent={parent}
 	    			items={itemState.items}
 	    			itemCount={itemState.itemCount}
 	    			listStart={itemState.listStart}
@@ -70,15 +78,12 @@ export default function PMReleaseView({containerState, itemState, appPrefs, onLi
 	    			onSearchClick={onSearchClick}
 	    			onPaginationClick={onPaginationClick}
 	    			onOrderBy={onOrderBy}
-	    			onHeader={onModify}
-	    			onOption1={onModify}
-	    			onOption2={openDeleteModal}
-	    			onOption3={onEditRoles}
+	    			onOption={onOption}
 	    			orderCriteria={itemState.orderCriteria}
 					searchCriteria={itemState.searchCriteria}
 	    		/>
     		)}
-    		<Modal isOpen={containerState.isDeleteModalOpen} onClose={closeModal()} >
+    		<Modal isOpen={containerState.isDeleteModalOpen} onClose={() => closeModal()} >
     			<div className="modal-dialog">
     				<div className="modal-content">
     					<div className="modal-header">
@@ -89,8 +94,8 @@ export default function PMReleaseView({containerState, itemState, appPrefs, onLi
     						<h3>Are you sure you want to delete?</h3>
     					</div>
     					<div className="modal-footer">
-    						<button type="button" className="btn btn-primary" onClick={onDelete(containerState.selected)}>Delete</button>
-    						<button type="button" className="btn btn-secondary" data-dismiss="modal" onClick={closeModal()}>Close</button>
+    						<button type="button" className="btn btn-primary" onClick={() => onOption("DELETEFINAL",containerState.selected)}>Delete</button>
+    						<button type="button" className="btn btn-secondary" data-dismiss="modal" onClick={() => closeModal()}>Close</button>
     					</div>
     				</div>
     			</div>
@@ -109,11 +114,9 @@ PMReleaseView.propTypes = {
   onSearchClick: PropTypes.func,
   onPaginationClick: PropTypes.func,
   onOrderBy: PropTypes.func, 
-  openDeleteModal: PropTypes.func,
   closeModal: PropTypes.func,
-  onModify: PropTypes.func,
-  onDelete: PropTypes.func,
-  onEditRoles: PropTypes.func,
+  onOption: PropTypes.func,
   inputChange: PropTypes.func,
+  goBack: PropTypes.func,
   session: PropTypes.object
 };
