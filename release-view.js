1e25ee6a87efcd0ee278cd040b557a3062957fa2
Edward Seufert <edwardseufert@Edwards-MBP.home>
2020-06-14 13:28:21 -0400

    workflow step buildout

--- release-view.js
+++ release-view.js
@@ -28,7 +28,7 @@ export default function PMReleaseView({containerState, itemState, appPrefs, onLi
 	}
 	
 	if (goBack != null && parent != null && parent != "") {
-		header = <div>{header} : <a onClick={() => goBack()} aria-hidden="true">{parent}</a></div>;
+		header = <span>{header} : <a onClick={() => goBack()} aria-hidden="true">{parent}</a></span>;
 	}
     
 	let deleteModalHeader = "Delete ";
